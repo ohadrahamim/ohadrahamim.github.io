5968351aeaebca66d87a3e7da3f79a9303f19861
ohad204 <rahamiob@biu.ac.il>
2024-06-03 11:55:10 +0300

    my home-page

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Post: Real-Time Semantic Segmentation",
+        "excerpt":"#ENet model implementation In this blog I will present an overview on image segmentation and the reproduction of ENet paper. Link to the paper: https://arxiv.org/pdf/1606.02147.pdf Link to the repository: https://github.com/iArunava/ENet-Real-Time-Semantic-Segmentation Image Segmentation Overview In computer vision, image segmentation is the process of partitioning a digital image into multiple segments (sets...","categories": ["blog"],
+        "tags": ["Computer Vision","Machine Learning","Deep Learning"],
+        "url": "/blog/enet/",
+        "teaser": null
+      }]
